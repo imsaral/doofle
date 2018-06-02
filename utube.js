820bcc114260c06be6d2f1b54d858822dc7ecf97
Saral Agarwal <im.sar.agr@gmail.com>
2018-06-03 00:46:49 +0530

    a commit

--- utube.js
+++ utube.js
@@ -1,27 +1,34 @@
-var Obj
-var ustr="dog"
+var uObj
+var ustr="ronaldo"
 function ureqListener(){
   //console.log('herev also')
-  //console.log(this.responseText)
-  Obj=JSON.parse(this.responseText)
-  let x=Obj.items[0].id.videoId
+  console.log(this.responseText)
+  uObj=JSON.parse(this.responseText)
+  for(i=0;i<10;i++){
+    let x=uObj.items[i].id.videoId
   //console.log(Obj.items[0].id.videoId)
-  let a=document.createElement('iframe')
-  a.setAttribute("width","560")
-  a.setAttribute("height","315")
-  a.setAttribute("src",`https://www.youtube.com/embed/${x}`)
-  a.setAttribute("frameborder","0")
-  a.setAttribute("allow","autoplay;encrypted-media")
-  a.setAttribute("allowfullscreen","true")
-  let b=document.getElementById('maincontain')
-  b.append(a)
+    let d=document.createElement('div')
+    let h=document.createElement('h5')
+    h.innerHTML=uObj.items[i].snippet.title
+    let a=document.createElement('iframe')
+  //a.setAttribute("width","560")
+  //a.setAttribute("height","315")
+    a.setAttribute("src",`https://www.youtube.com/embed/${x}`)
+    a.setAttribute("frameborder","0")
+    a.setAttribute("allow","autoplay;encrypted-media")
+    a.setAttribute("allowfullscreen","true")
+    let b=document.getElementById('video')
+    d.append(h)
+    d.append(a)
+    b.append(d)
+  }
 }
 let ui=setInterval(function uf(){
   //console.log('gere')
-  var oReq=new XMLHttpRequest()
-  oReq.open("GET",`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${ustr}&type=video&videoCaption=closedCaption&key=AIzaSyBgRBtfg3im4_6a3XGq3r4UcLJ0bweU6-4`)
-  var z=oReq.addEventListener("load",ureqListener)
-  oReq.send();                                            
+  var uoReq=new XMLHttpRequest()
+  uoReq.open("GET",`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${ustr}&maxResults=10&type=video&videoCaption=closedCaption&key=AIzaSyBgRBtfg3im4_6a3XGq3r4UcLJ0bweU6-4`)
+  var z=uoReq.addEventListener("load",ureqListener)
+  uoReq.send();                                            
 },500)
 setTimeout(function g(){
   clearInterval(ui)
